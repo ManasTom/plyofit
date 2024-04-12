b32f93b5437f3c6af367f54141e95e33ac30a8ab
ManasTom <manastom670@gmail.com>
2024-04-12 15:18:56 +0530

    about page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,28 +1,4 @@
-$(document).ready(function() {
-    var owl = $('.choose_us_section .owl-carousel');
-    owl.owlCarousel({
-        margin: 30,
-        nav: true,
-        loop: true,
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 4500,
-        responsive: {
-            0: {
-                items: 1
-            },
-            576: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            992: {
-                items: 4
-            }
-        }
-    })
-})
+
 
 $(document).ready(function() {
     var owl = $('.our_news_section .owl-carousel');
