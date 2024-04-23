5410e284a7943c3ba9021ceff564954baa39b12b
ManasTom <manastom670@gmail.com>
2024-04-23 10:45:04 +0530

    x

---
+++ assets/js/admin.js
@@ -0,0 +1,43 @@
+//script to toggle between careers and certifications window in admin pannel
+document.addEventListener("DOMContentLoaded", function() {
+    const careersBarMobile = document.querySelector(".admin_sidebar_mobile_careers_bar");
+    const certificationsBarMobile = document.querySelector(".admin_sidebar_mobile_Certifications_bar");
+    const careersBar = document.querySelector(".admin_sidebar_careers_bar");
+    const certificationsBar = document.querySelector(".admin_sidebar_Certifications_bar");
+    const careersAdminBoard = document.querySelector(".careers_admin_board");
+    const certificationsAdminBoard = document.querySelector(".certifications_admin_board");
+
+    careersBarMobile.addEventListener("click", function() {
+        careersAdminBoard.style.display = "block";
+        certificationsAdminBoard.style.display = "none";
+    });
+
+    certificationsBarMobile.addEventListener("click", function() {
+        certificationsAdminBoard.style.display = "block";
+        careersAdminBoard.style.display = "none";
+    });
+
+    careersBar.addEventListener("click", function() {
+        careersAdminBoard.style.display = "block";
+        certificationsAdminBoard.style.display = "none";
+    });
+
+    certificationsBar.addEventListener("click", function() {
+        certificationsAdminBoard.style.display = "block";
+        careersAdminBoard.style.display = "none";
+    });
+});
+
+
+
+var job_id, vaccancy_name, job_description, experience, deadline;
+
+function ReadForm() {
+    job_id = document.getElementById("job_id").value;
+    
+}
+
+
+document.getElementById("create").onclick = function(){
+    ReadForm();
+}
