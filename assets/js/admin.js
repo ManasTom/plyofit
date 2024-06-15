0f58c988360f359986c919bb3a6dfce55cb8c2dc
ManasTom <manastom670@gmail.com>
2024-06-15 17:41:59 +0530

    UPDATES

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1111,6 +1111,7 @@ function downloadEncryptedJSON() {
 // ********************************************************************************************
 
 window.onload = function () {
+    console.log('***')
     // Function to handle user logout
     function logoutUser() {
         console.log("Logging out user due to inactivity or tab/window close");
